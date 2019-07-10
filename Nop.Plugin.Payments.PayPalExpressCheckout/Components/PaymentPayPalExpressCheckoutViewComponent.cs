c18cdb492c922a8d305e1056cf833551639efb7c
skoshelev <serega.dn@gmail.com>
2019-07-10 16:33:27 +0300

    #8 Added checking restrictions by country

--- Nop.Plugin.Payments.PayPalExpressCheckout/Components/PaymentPayPalExpressCheckoutViewComponent.cs
+++ Nop.Plugin.Payments.PayPalExpressCheckout/Components/PaymentPayPalExpressCheckoutViewComponent.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Common;
 using Nop.Plugin.Payments.PayPalExpressCheckout.Models;
 using Nop.Plugin.Payments.PayPalExpressCheckout.Services;
 using Nop.Services.Orders;
+using Nop.Services.Payments;
 using Nop.Web.Framework.Components;
 
 namespace Nop.Plugin.Payments.PayPalExpressCheckout.Components
@@ -9,14 +12,23 @@ namespace Nop.Plugin.Payments.PayPalExpressCheckout.Components
     [ViewComponent(Name = "PaymentPayPalExpressCheckout")]
     public class PaymentPayPalExpressCheckoutViewComponent : NopViewComponent
     {
+        private readonly AddressSettings _addressSettings;
         private readonly IOrderProcessingService _orderProcessingService;
+        private readonly IPaymentPluginManager _paymentPluginManager;
         private readonly IPayPalExpressCheckoutService _payPalExpressCheckoutService;
+        private readonly IWorkContext _workContext;
 
-        public PaymentPayPalExpressCheckoutViewComponent(IOrderProcessingService orderProcessingService,
-            IPayPalExpressCheckoutService payPalExpressCheckoutService)
+        public PaymentPayPalExpressCheckoutViewComponent(AddressSettings addressSettings,
+            IOrderProcessingService orderProcessingService,
+            IPaymentPluginManager paymentPluginManager,
+            IPayPalExpressCheckoutService payPalExpressCheckoutService,
+            IWorkContext workContext)
         {
+            _addressSettings = addressSettings;
             _orderProcessingService = orderProcessingService;
+            _paymentPluginManager = paymentPluginManager;
             _payPalExpressCheckoutService = payPalExpressCheckoutService;
+            _workContext = workContext;
         }
 
         public IViewComponentResult Invoke()
@@ -27,6 +39,15 @@ namespace Nop.Plugin.Payments.PayPalExpressCheckout.Components
 
             var minOrderSubtotalAmountOk = _orderProcessingService.ValidateMinOrderSubtotalAmount(cart);
             if (!minOrderSubtotalAmountOk)
+                return Content(string.Empty);
+            
+            var filterByCountryId = 0;
+            if (_addressSettings.CountryEnabled && _workContext.CurrentCustomer.BillingAddress?.Country != null)
+                filterByCountryId = _workContext.CurrentCustomer.BillingAddress.Country.Id;
+
+            var plugin = _paymentPluginManager.LoadPluginBySystemName("Payments.PayPalExpressCheckout");
+
+            if(plugin == null || _paymentPluginManager.GetRestrictedCountryIds(plugin).Contains(filterByCountryId))
                 return Content(string.Empty);
 
             var model = new PaymentInfoModel
